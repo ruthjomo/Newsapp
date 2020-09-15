756c88ea0e6120216f81acee18f4ca8351495b68
ruthjomo <ruthjomo19@gmail.com>
2020-09-15 17:50:21 +0300

    indented codes

--- tests/sources_test.py
+++ tests/sources_test.py
@@ -2,14 +2,14 @@ import unittest
 from app.models import Source
 
 class SourceTest(unittest.TestCase):
-    '''
-    Test Class to test the behaviour of the Source class
-    '''
+    
+    
+    
     def setUp(self):
 
-        '''
-        Set up method that will run before every Test
-        '''
+        
+        
+        
 
         self.new_source = Source("abc-news","ABC News","Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.", "https://abcnews.go.com", "general", "en", "us")
     
